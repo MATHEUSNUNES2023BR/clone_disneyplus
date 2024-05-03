66fdc2c744c766fd8c850971406be5db5eb44dea
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-05-03 16:53:42 -0300

    continuando

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')('sass')
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
+
 function styles(){
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+    return gulp.src('./src/styles/*.scss', { encoding: false })
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'))
 }
-exports.default = styles
+    
+function images(){
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images)
+exports.watch =  function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
